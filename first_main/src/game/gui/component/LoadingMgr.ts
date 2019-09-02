14f709850c6027940c4945d187a97348f1372f32
happy <happy>
2019-09-02 20:34:45 +0800

    还没加载出来时去掉转圈

--- first_main/src/game/gui/component/LoadingMgr.ts
+++ first_main/src/game/gui/component/LoadingMgr.ts
@@ -99,11 +99,11 @@ module game.gui.component {
 					if (ignore && ignore.indexOf(key) != -1) continue;
 					let assertloader = this._assetsLoader[key];
 					if (assertloader) {
-						if (ignore && ignore.indexOf("dating") != -1) {
-							assertloader.forceMoveRef();
-						} else {
-							assertloader.clear(true);
-						}
+						assertloader.forceMoveRef();
+						// if (ignore && ignore.indexOf("dating") != -1) {
+						// } else {
+						// 	assertloader.clear(true);
+						// }
 						assertloader.clearAssert();
 						assertloader = null;
 					}
