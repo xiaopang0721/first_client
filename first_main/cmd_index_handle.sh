d2e44e52bdb33c13d6f5fca05dac6e5017c72c39
xiaopang <xiaopang>
2019-09-16 13:27:50 +0800

    脚本提交

--- first_main/cmd_index_handle.sh
+++ first_main/cmd_index_handle.sh
@@ -41,15 +41,15 @@ index_libs="$bin/index_libs"
 temp="$__root/temp.txt"
 mytemp="$__root/mytemp.txt"
 #获取关键行号
-startRow=`cat $indx | grep -En 'startTagAAA|endTagAAA' | awk -F: '{print $1}' | sed -n '1p'`
-endRow=`cat $indx | grep -En 'startTagAAA|endTagAAA' | awk -F: '{print $1}' | sed -n '2p'`
+startRow=`cat $indx | grep -En 'startTagAAA' | awk -F: '{print $1}'`
+endRow=`cat $indx | grep -En 'endTagAAA' | awk -F: '{print $1}'`
 startRow=$[startRow+1]
 endRow=$[endRow-1]
 echo $startRow
 echo $endRow
 
-startCustomRow=`cat $indx | grep -En 'Custom' | awk -F: '{print $1}' | sed -n '1p'`
-endCustomRow=`cat $indx | grep -En 'Custom' | awk -F: '{print $1}' | sed -n '2p'`
+startCustomRow=`cat $indx | grep -En 'startCustom' | awk -F: '{print $1}'`
+endCustomRow=`cat $indx | grep -En 'endCustom' | awk -F: '{print $1}'`
 startCustomRow=$[startCustomRow+1]
 endCustomRow=$[endCustomRow-1]
 echo $startCustomRow
@@ -75,7 +75,7 @@ for d in ${game_list[@]}; do
 		echo "txt: $txt"
 		echo `cat $txt`>>$temp
 		if [ "$d" != "game" ];then
-			echo '<script src="js/'$d'/MyInport.js"></script>\n'>>$mytemp
+			echo '<script-src="js/'$d'/MyInport.js"></script>\n'>>$mytemp
 		fi
 	fi
 done
