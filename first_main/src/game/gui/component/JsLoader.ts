3e6234b7c031813b20193a6c21632faa0476d064
xiaopang <xiaopang>
2019-09-19 19:00:04 +0800

    还原

--- first_main/src/game/gui/component/JsLoader.ts
+++ first_main/src/game/gui/component/JsLoader.ts
@@ -65,8 +65,8 @@ module game.gui.component {
 					let dataStr = path.indexOf(".bin") != -1 ? StringU.readZlibData(new ByteArray(tempData)) : tempData;
 					let script = document.createElement('script');
 					// script.type = "module";
-					script.async = false;
-					script.defer = true;
+					// script.async = false;
+					// script.defer = true;
 					script.innerHTML = dataStr;
 					document.body.appendChild(script);
 					if (gameid.indexOf("dating") == -1 && gameid.indexOf("component") == -1 && gameid.indexOf("tongyong") == -1) {
