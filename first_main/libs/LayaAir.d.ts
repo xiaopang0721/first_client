d8beb11a97fd38253e04ec28d941bb1523d61bb3
xiaopang <xiaopang>
2019-10-15 20:39:04 +0800

    api接口

--- first_main/libs/LayaAir.d.ts
+++ first_main/libs/LayaAir.d.ts
@@ -17515,6 +17515,15 @@ declare module laya.events {
          */
         on(type: string, caller: any, listener: Function, args?: Array<any>): EventDispatcher;
         /**
+         * 使用 EventDispatcher 对象注册指定类型的事件侦听器对象，以使侦听器能够接收事件通知。
+         * @param type		事件的类型。
+         * @param caller	事件侦听函数的执行域。
+         * @param listener	事件侦听函数。
+         * @param args		（可选）事件侦听函数的回调参数。
+         * @return 此 EventDispatcher 对象。
+         */
+        onAPI(type: string, caller: any, listener: Function, args?: Array<any>): EventDispatcher;
+        /**
          * 使用 EventDispatcher 对象注册指定类型的事件侦听器对象，以使侦听器能够接收事件通知，此侦听事件响应一次后自动移除。
          * @param type		事件的类型。
          * @param caller	事件侦听函数的执行域。
@@ -17533,6 +17542,15 @@ declare module laya.events {
          * @return 此 EventDispatcher 对象。
          */
         off(type: string, caller: any, listener: Function, onceOnly?: boolean): EventDispatcher;
+        /**
+         * 从 EventDispatcher 对象中删除侦听器。
+         * @param type		事件的类型。
+         * @param caller	事件侦听函数的执行域。
+         * @param listener	事件侦听函数。
+         * @param onceOnly	（可选）如果值为 true ,则只移除通过 once 方法添加的侦听器。
+         * @return 此 EventDispatcher 对象。
+         */
+        offAPI(type: string, caller: any, listener: Function, onceOnly?: boolean): EventDispatcher;
         /**
          * 从 EventDispatcher 对象中删除指定事件类型的所有侦听器。
          * @param type	（可选）事件类型，如果值为 null，则移除本对象所有类型的侦听器。
